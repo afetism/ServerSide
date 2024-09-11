d9a75e9a88017917a1fbefa041814be5aab852c0
afetism <aismayilova1215@gmail.com>
2024-09-11 12:02:19 +0400

    Add project files.

---
+++ ClinteSide/MainViewModel.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ClinteSide;
+
+class MainViewModel : INotifyPropertyChanged
+{
+    private string ip = null!;
+    private int port;
+    private string image=null!;
+
+    public string Ip { get => ip; set { ip = value; OnPropertyChanged(); } }
+    public int Port { get => port; set { port = value; OnPropertyChanged(); } }
+    public string Image { get => image; set { image = value; OnPropertyChanged(); } }
+    public RelayCommand SelectFileCommand { get; set; }
+    public RelayCommand Send{ get; set; }
+    private string SelectFile()
+    {
+        var dlg = new OpenFileDialog()
+        {
+            Filter = "JPEG Files (*.jpeg;*.jpg) | *.jpeg;*.jpg",
+            RestoreDirectory = true
+        };
+
+        if (dlg.ShowDialog() != true)
+            return "";
+        else return dlg.FileName;
+    }
+    
+
+    public MainViewModel()
+    {
+        SelectFileCommand = new(executeFile);
+        Send = new(executeSend);
+    }
+
+    static void SendMethod(NetworkStream netstr, byte[] message)
+    {
+        try
+        {
+            netstr.Write(message, 0, message.Length);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error!\n" + ex.Message + "\n" + ex.StackTrace);
+        }
+    }
+
+    private void executeSend(object obj)
+    {
+        using var client = new TcpClient();
+        var ip = IPAddress.Parse(Ip);
+        var ep = new IPEndPoint(ip, Port);
+        try
+        {
+            client.Connect(ep);
+            if (client.Connected)
+            {
+                using (var stream = client.GetStream()) 
+                {
+                    using (var source = new FileStream(Image, FileMode.Open, FileAccess.Read))
+                    {
+                        byte[] buffer = new byte[1024]; 
+                        int bytesRead;
+                        while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            SendMethod(stream, buffer.Take(bytesRead).ToArray()); 
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error occurred: {ex.Message}\n{ex.StackTrace}");
+        }
+    }
+
+
+    private void executeFile(object obj)
+    {
+        Image = SelectFile();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ ServerSide/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace ServerSide;
+
+public class MainViewModel:INotifyPropertyChanged
+{
+    private string image;
+
+    public string Image 
+    { get => image; set
+        {
+            image = value;
+            OnPropertyChanged();
+        }
+        }
+    static TcpListener listener;
+    public MainViewModel()
+    {
+        listener = new TcpListener(IPAddress.Parse("10.1.18.9"), 2701);
+        listener.Start();
+        var client = new Server.Net.Client(listener.AcceptTcpClient());
+        var task = Task.Run(() =>
+        {
+            Console.WriteLine($"{client.Client.RemoteEndPoint} connected");
+           
+        });
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+}
